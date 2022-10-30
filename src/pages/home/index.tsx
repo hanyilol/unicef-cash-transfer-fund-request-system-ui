68f251c811f8bcc27bbd0018d2dbdbb1411a1c45
hanyi <hanyi.du.cs@gmail.com>
2022-10-30 17:38:23 +0000

    add home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled";
+
+const Wrapper = styled.main`
+  display: flex;
+  position: relative;
+  width: 100%;
+  min-height: calc(100vh - 6rem);
+  align-items: center;
+  justify-content: center;
+`;
+
+const Intro = styled.div`
+  margin-bottom: 12rem;
+  text-align: center;
+  h1 {
+    font-size: 2rem;
+  }
+  p {
+    font-size: 1.5rem;
+  }
+
+  @media screen and (max-width: ${({ theme }) => `${theme.mediaWidth.upToExtraSmall}px`}) {
+    h1 {
+      font-size: 3rem;
+    }
+    p {
+      font-size: 0.8rem;
+    }
+  }
+`;
+
+const Line = styled.div`
+  width: 8rem;
+  border-bottom: 2px solid ${({ theme }) => theme.colors.normal};
+  margin: 0 auto;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  @media screen and (max-width: ${({ theme }) => `${theme.mediaWidth.upToExtraSmall}px`}) {
+    width: 4rem;
+  }
+`;
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <Intro>
+        <h1>Unicef Cash Transfer</h1>
+        <Line />
+        <p>Request System</p>
+      </Intro>
+    </Wrapper>
+  );
+}
